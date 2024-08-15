70065f6828fe0ad0bf1c3f2c86d5bed71201f477
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-15 17:27:45 +0530

    Create user req

---
+++ backend/src/main/java/Employee/Managment/System/backend/Request/CreateUserRequest.java
@@ -0,0 +1,10 @@
+package Employee.Managment.System.backend.Request;
+
+import lombok.Data;
+
+@Data
+public class CreateUserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
