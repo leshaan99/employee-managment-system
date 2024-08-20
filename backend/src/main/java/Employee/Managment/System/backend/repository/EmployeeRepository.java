b88a1424cff76ca59972482772835b9b90a268e8
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-21 01:42:23 +0530

    Update application.properties

--- backend/src/main/java/Employee/Managment/System/backend/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package Employee.Managment.System.backend.repository;
-
-import Employee.Managment.System.backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
--- backend/src/main/java/Employee/Managment/System/backend/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package Employee.Managment.System.backend.service;
-
-import Employee.Managment.System.backend.entity.Employee;
-
-public interface EmployeeService {
-    public Employee createEmployee(Employee employee);
-}
