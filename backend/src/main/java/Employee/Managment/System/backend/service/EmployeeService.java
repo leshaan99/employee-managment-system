b329a7b8eb6d35f16bb997e7c3249e47122400d8
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-21 02:31:54 +0530

    Update pom.xml

--- backend/src/main/java/Employee/Managment/System/backend/service/EmployeeService.java
+++ backend/src/main/java/Employee/Managment/System/backend/service/EmployeeService.java
@@ -2,6 +2,7 @@ package Employee.Managment.System.backend.service;
 
 import Employee.Managment.System.backend.dto.EmployeeDTO;
 import Employee.Managment.System.backend.repository.EmployeeRepo;
+import Employee.Managment.System.backend.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmployeeService {
@@ -12,9 +13,9 @@ public class EmployeeService {
     public String saveEmployee(EmployeeDTO employeeDTO){
 
         if(employeeRepo.existsById(employeeDTO.getEmpId())){
-            return
+            return VarList.RSP_DUPLICATED;
         }else {
-
+            employeeRepo.save(employeeDTO)
         }
     }
 }
