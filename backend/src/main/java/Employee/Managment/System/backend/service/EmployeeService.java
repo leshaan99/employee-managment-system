28733795a0e5a1829f3b2d8f7feb1129d3e48c39
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-21 02:39:48 +0530

    Update employee service

--- backend/src/main/java/Employee/Managment/System/backend/service/EmployeeService.java
+++ backend/src/main/java/Employee/Managment/System/backend/service/EmployeeService.java
@@ -1,21 +1,31 @@
 package Employee.Managment.System.backend.service;
 
 import Employee.Managment.System.backend.dto.EmployeeDTO;
+import Employee.Managment.System.backend.entity.Employee;
 import Employee.Managment.System.backend.repository.EmployeeRepo;
 import Employee.Managment.System.backend.util.VarList;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class EmployeeService {
 
     @Autowired
     private EmployeeRepo employeeRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public String saveEmployee(EmployeeDTO employeeDTO){
 
         if(employeeRepo.existsById(employeeDTO.getEmpId())){
             return VarList.RSP_DUPLICATED;
         }else {
-            employeeRepo.save(employeeDTO)
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
         }
     }
 }
