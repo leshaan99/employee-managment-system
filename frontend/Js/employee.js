ed0de9eb0b8570e951f84184b9ef838e9a4bcacc
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-24 16:13:28 +0530

    Update save employee function

--- frontend/Js/employee.js
+++ frontend/Js/employee.js
@@ -3,5 +3,22 @@ function saveEmployee(){
     let address=$('#empaddress').val();
     let number=$('#empnum').val()
 
-    console.log(name);
+    $.ajax({
+        method: "POST",
+        contentType: "application/json",
+        url: "http://localhost:8080/api/employee/saveEmployee",
+        async:true,
+        data:JSON.stringify({
+                "empId" : "",
+                "empName" : name,
+                "empAddress" : address,
+                "empMNum" : number
+        }),
+        success:function(data){
+            alert("Employee saved successfully")
+        },
+        error:function(xhr, exception){
+            alert("Error")
+        }
+    })
 }
